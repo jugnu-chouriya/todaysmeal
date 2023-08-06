520fde90895d85ceefb81a140ddaf9f1e291db61
jugnu-chouriya <jugnuc3@gmail.com>
2023-08-07 01:20:23 +0530

    again updated the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-get '/', to: 'home#index'
+get '/', to: 'application#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
