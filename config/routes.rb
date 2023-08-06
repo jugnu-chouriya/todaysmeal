71e4811d8afd126cee0954ddc6750c00072d14a6
jugnu-chouriya <jugnuc3@gmail.com>
2023-08-07 01:31:43 +0530

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-get '/', to: 'application#index'
+get '/', to: 'about#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
