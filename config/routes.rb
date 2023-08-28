fa25d84865c7a91d2b1538fd04ce07db4b2cd4da
jugnu <jugnuc3@gmail.com>
2023-08-28 17:26:33 +0530

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+root 'dashboard#index'
 get '/', to: 'about#index'
+get '/dashboard', to: 'dashboard#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
